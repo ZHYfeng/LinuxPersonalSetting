a09ce602e0f7ac5d0b86a6ca73e17bdd478a1fe3
Yu <yhao016@ucr.edu>
2021-02-21 20:22:22 -0800

    add lld

--- set_llvm.bash
+++ set_llvm.bash
@@ -10,7 +10,7 @@ else
     git clone https://github.com/llvm/llvm-project.git
     mkdir $LLVM/build
     cd $LLVM/build
-    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_INSTALL_PREFIX=/home/yhao/data/build -DLLVM_TARGETS_TO_BUILD="X86;AArch64" ../llvm
+    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;lld" -DCMAKE_INSTALL_PREFIX=/home/yhao/data/build -DLLVM_TARGETS_TO_BUILD="X86;AArch64" ../llvm
     make -j10
     make install
 fi
