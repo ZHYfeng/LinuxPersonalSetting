637c4fcf579b56ae88bc9ea99a3f3cfd6daa8c69
Yu <yhao016@ucr.edu>
2021-03-27 17:18:50 -0700

    update llvm and golang

--- set_llvm.bash
+++ set_llvm.bash
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-BASE=$HOME/data/git
+BASE=$HOME/data/2021-Template/third_parts
 BUILD=$BASE/../build
 LLVM=$BASE/llvm-project
 if [ -d "$LLVM" ]; then
     echo "$LLVM exist"
 else
-    cd $BASE || echo "$BASE not exit" && exit
+    echo "$LLVM not exist"
+    cd $BASE || exit
     git clone https://github.com/llvm/llvm-project.git
-    cd $LLVM || echo "$LLVM not exit" && exit
+    cd $LLVM || exit
     git checkout tags/llvmorg-11.0.1
-    mkdir $LLVM/build && cd $LLVM/build || echo "$LLVM/build not exit" && exit
+    mkdir $LLVM/build && cd $LLVM/build || exit
     cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS="clang;lld" -DCMAKE_INSTALL_PREFIX=$BUILD -DLLVM_TARGETS_TO_BUILD="X86;AArch64" ../llvm
     make -j10
     make install
